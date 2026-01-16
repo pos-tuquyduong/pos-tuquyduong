7c0990db94e8d2cd1c2be84eb160ebe1ce7f1eed
tuquyduong <tuquyduong83@gmail.com>
2026-01-16 06:11:31 +0000

    Tăng giới hạn upload size lên 5MB

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Request logging
